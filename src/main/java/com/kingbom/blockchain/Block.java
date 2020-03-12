e8d568b4510085e8733bd930ac083f4f6da35cc5
jaruwit suriyo <jaruwit.sur@ascendcorp.com>
2020-03-13 01:25:30 +0700

    [Add] new pojo class Block and add lombok lib

--- src/main/java/com/kingbom/blockchain/Block.java
+++ src/main/java/com/kingbom/blockchain/Block.java
@@ -1,4 +1,16 @@
 package com.kingbom.blockchain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Block {
+    private String hash;
+    private String previousHash;
+    private String data;
+    private long timeStamp;
+    private int nonce;
 }
